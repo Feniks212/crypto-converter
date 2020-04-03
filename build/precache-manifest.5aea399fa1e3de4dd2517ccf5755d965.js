997bb8dc4f85178903ccadc2f2daaa691c67d587
Feniks212 <Vovanchik211993@gmail.com>
2020-04-03 23:32:49 +0300

    add build

---
+++ build/precache-manifest.5aea399fa1e3de4dd2517ccf5755d965.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9f7aad249676302713ec4090ddacc9d6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "767596937fbec43929f2",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "b7a834cf28af8cf4a6fd",
+    "url": "/static/js/2.2c9cf00c.chunk.js"
+  },
+  {
+    "revision": "882fee5ad56cfd78b1cc5d94b617aa42",
+    "url": "/static/js/2.2c9cf00c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "767596937fbec43929f2",
+    "url": "/static/js/main.70d88d1d.chunk.js"
+  },
+  {
+    "revision": "04ac66080a33b90a734d",
+    "url": "/static/js/runtime-main.79e75378.js"
+  }
+]);
